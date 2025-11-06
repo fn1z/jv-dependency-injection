e35feaafec5425e61405b832a5a4eab9090e1f55
fn1z <mr.artemrud2005@gmai.com>
2025-11-06 12:11:02 +0200

    Replaced many exceptions that have a common parent with a general parental exception

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,24 +1,22 @@
 package mate.academy.lib;
 
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Injector {
-    private static final String CLASS = "Class";
+    private static final Injector injector = new Injector();
     private static final String IMPL = "Impl";
+    private static final String CLASS = "Class";
     private static final String SPACE = " ";
 
-    private static final Injector injector = new Injector();
-
-    private static final Map<Class<?>, Class<?>> interfaceToImplementation = Map.of(
+    private final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+            mate.academy.service.ProductService.class,
+            mate.academy.service.impl.ProductServiceImpl.class,
             mate.academy.service.FileReaderService.class,
             mate.academy.service.impl.FileReaderServiceImpl.class,
             mate.academy.service.ProductParser.class,
-            mate.academy.service.impl.ProductParserImpl.class,
-            mate.academy.service.ProductService.class,
-            mate.academy.service.impl.ProductServiceImpl.class
+            mate.academy.service.impl.ProductParserImpl.class
     );
 
     private final Map<Class<?>, Object> instances = new HashMap<>();
@@ -31,42 +29,43 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        try {
-            Class<?> implClass = findImplementation(interfaceClazz);
+        if (instances.containsKey(interfaceClazz)) {
+            return instances.get(interfaceClazz);
+        }
 
+        Class<?> implClass;
+        try {
+            implClass = findImplementation(interfaceClazz);
             if (!implClass.isAnnotationPresent(Component.class)) {
                 throw new RuntimeException(CLASS + SPACE + implClass.getName()
                         + " is missing @Component annotation");
             }
 
-            if (instances.containsKey(implClass)) {
-                return instances.get(implClass);
-            }
-
-            Constructor<?> constructor = implClass.getDeclaredConstructor();
-            constructor.setAccessible(true);
-            Object implInstance = constructor.newInstance();
-
-            for (Field field : implClass.getDeclaredFields()) {
-                if (field.isAnnotationPresent(Inject.class)) {
-                    Object dependency = getInstance(field.getType());
-                    field.setAccessible(true);
-                    field.set(implInstance, dependency);
-                }
-            }
-
-            instances.put(implClass, implInstance);
+            Object implInstance = createInstance(implClass);
+            instances.put(interfaceClazz, implInstance);
             return implInstance;
-
-        } catch (Exception e) {
-            throw new RuntimeException("Can't create instance of " + interfaceClazz.getName(), e);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Can't create instance of class: "
+                    + interfaceClazz.getName(), e);
         }
     }
 
-    private Class<?> findImplementation(Class<?> interfaceClazz) {
-        if (interfaceClazz.isInterface()) {
-            return interfaceToImplementation.get(interfaceClazz);
+    private Object createInstance(Class<?> implClass) throws ReflectiveOperationException {
+        Object instance = implClass.getDeclaredConstructor().newInstance();
+        for (Field field : implClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Inject.class)) {
+                Object fieldInstance = getInstance(field.getType());
+                field.setAccessible(true);
+                field.set(instance, fieldInstance);
+            }
         }
-        return interfaceClazz;
+        return instance;
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (interfaceImplementations.containsKey(interfaceClazz)) {
+            return interfaceImplementations.get(interfaceClazz);
+        }
+        throw new RuntimeException("Can't find implementation for: " + interfaceClazz.getName());
     }
 }
